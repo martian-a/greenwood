f91f14cc5644bbecef800f701f2ce8606271809b
martian-a <coder@bluegumtree.com>
2017-06-06 18:04:52 +0100

    Pushing static version of latest analysis to github pages.

---
+++ assets/js/update.js
@@ -0,0 +1,27 @@
+function update($) {
+	
+	$("div.network").append("<button id=\"save-network\">Update Saved Layout</button>");
+	$("#ticket-distribution").append("<button id=\"save-tickets\">Update Saved Layout</button>");
+	
+	function getData(network) {
+	
+		var positions = network.getPositions();
+		var json = JSON.stringify(positions);
+		
+		$.post(
+			"/exist/apps/greenwood/utils/update/network.xq?game=" + gameId, 
+			"json=" + json, 
+			function(data, status){
+				alert(status),
+			'xml';
+		});
+	};
+	
+	$("#save-network").on("click", function(){
+		getData(routesNetwork.getNetwork());
+	});
+	$("#save-tickets").on("click", function(){
+		getData(ticketsNetwork.getNetwork());		
+	});
+	
+};
